aaefbbbe8aa8557941511e84b8d850ec3237df28
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2025-03-31 21:47:14 +0530

    Add shadcn to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <>
-      <div className="mb-4 p-4 text-3xl">App</div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 };
